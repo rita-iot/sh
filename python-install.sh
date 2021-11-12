03a6301c44967b77af1d868284a06a7ffce97585
小菜鸟 <67440@163.com>
2021-11-12 14:46:05 +0800

    添加安装python3脚本

--- python-install.sh
+++ python-install.sh
@@ -8,9 +8,14 @@
 echo "安装依赖--------------------------------------------->"
 yum -y groupinstall "Development tools"
 yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel
-if [ -f "Python-3.6.2.tar.xz"];then
-	echo "下载------------------------------------------------->"
-	wget https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tar.xz
+echo "下载------------------------------------------------->"
+wget https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tar.xz
+# 如果不存在，启用备用下载链接
+if [ ! -e "Python-3.6.2.tar.xz" ];then
+	echo "下载失败，启用备用链接------------------------------------------------->"
+	wget https://cn.upy.workers.dev/install/python3/Python-3.6.2.tar.xz
+else
+	echo "存在------------------------------------------------->"
 fi
 echo "安装------------------------------------------------->"
 mkdir /usr/local/python3
